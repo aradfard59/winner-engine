6e821294ac41fdf5f17d62f0686e0ede7b79c9bd
aradfard59 <aradfard59@gmail.com>
2026-07-15 01:53:48 +0330

    Add v3 DOM Manager

--- assets/js/winner-engine-v3.js
+++ assets/js/winner-engine-v3.js
@@ -24,18 +24,92 @@ const WinnerEngine = {
     },
 
 
-    dom:{},
+    dom:{
+
+
+    layout:null,
+
+    viewer:null,
+
+    accordion:null,
+
+    rail:null,
+
+    indicator:null,
+
+
+    views:[],
+
+
+    sections:[]
+
+
+},
 
 
     init(){
 
 
-        console.log(
-            "Winner Engine v3 initialized"
+    this.cacheDOM();
+
+
+    console.log(
+        "Winner Engine v3 initialized"
+    );
+
+
+},
+
+
+cacheDOM(){
+
+
+    this.dom.layout =
+        document.querySelector(
+            ".winner-layout"
         );
 
 
-    }
+    if(!this.dom.layout) return;
+
+
+    this.dom.viewer =
+        this.dom.layout.querySelector(
+            ".winner-viewer"
+        );
+
+
+    this.dom.accordion =
+        this.dom.layout.querySelector(
+            ".winner-accordion"
+        );
+
+
+    this.dom.rail =
+        this.dom.layout.querySelector(
+            ".winner-rail"
+        );
+
+
+    this.dom.indicator =
+        this.dom.layout.querySelector(
+            ".winner-indicator"
+        );
+
+
+    this.dom.views =
+        [...this.dom.layout.querySelectorAll(
+            ".winner-view"
+        )];
+
+
+    this.dom.sections =
+        [...this.dom.layout.querySelectorAll(
+            ".winner-section"
+        )];
+
+
+}
 
 
 };
